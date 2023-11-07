018c1be5c9b348e980b9e52c5ddf6eab9a4a93d5
Parsanta Lal <89461899+Parsanta@users.noreply.github.com>
2023-11-07 22:13:19 +0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::thread;
 
 fn main() -> std::io::Result<()> {
     let server_address = "127.0.0.1:12345";
+    
+    //binding the client to the server address
     let client_socket = UdpSocket::bind("0.0.0.0:0")?;
     let server_addr: SocketAddr = server_address.parse().expect("Invalid server address");
     client_socket
@@ -14,8 +16,11 @@ fn main() -> std::io::Result<()> {
     client_socket.send("ping".as_bytes())?;
 
     println!("Welcome to Hangman!");
+    
+    //receiving game updates from the server
     let receive_updates_socket = client_socket.try_clone()?;
     let server_addr_clone = server_addr;
+    // thread to handle all the incomming updated scores 
     let receive_thread = thread::spawn(move || loop {
         match receive_updates_socket.recv_from(&mut buffer) {
             Ok((received, _server_address)) => {
@@ -31,6 +36,8 @@ fn main() -> std::io::Result<()> {
             }
         }
     });
+    
+    //Handles the input user and send it back to the server
     loop {
         let send_thread = thread::spawn(move || {
             let mut user_input = String::new();
